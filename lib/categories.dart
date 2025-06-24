cebb8a1330d82114885184ae175cf1d48408472d
Sadaen Parves Shoumik <sadaen.shoumik@northsouth.edu>
2025-06-24 12:15:55 +0600

    Project Setup & Display Categories Items On a Screen

---
+++ lib/categories.dart
@@ -0,0 +1,27 @@
+import "package:flutter/material.dart";
+import 'package:meals_app/widgets/category_grid_item.dart';
+import "package:meals_app/data/dummy_data.dart";
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pick Your Categories')),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: // availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
+            [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [category.color.withAlpha(77), category.color.withAlpha(220)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onSurface,
+        ),
+      ),
+    );
+  }
+}
