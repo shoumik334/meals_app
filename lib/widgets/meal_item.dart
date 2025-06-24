429712d29e3a270dafe85c3a47983bd1a8e6047c
Sadaen Parves Shoumik <sadaen.shoumik@northsouth.edu>
2025-06-24 23:37:41 +0600

    Stack Widget & Transparent Image

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {},
+        child: Stack(
+          // position multiple widgets directly above each other
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+            ),
+            Positioned(
+              
+              child: Container())
+          ],
+        ),
+      ),
+    );
+  }
+}
